f940c971bcfe23276d018385b6850da0749db626
Eric <eb11307@gmail.com>
2015-04-02 07:15:51 -0400

    made tcltk depends, moved defaults load to .onLoad

--- R/misc.R
+++ R/misc.R
@@ -6,19 +6,21 @@ defaultsPath <- normalizePath(paste0(system.file(package="raqdm"), "/defaults.rd
 baseURL <- "https://ofmext.epa.gov/AQDMRS/ws/"
 aqdm <- new.env()
 
-.onAttach <- function(...) {
+.onLoad <- function(...) {
 
   aqdmDefaults = NULL
   load(defaultsPath)
   options("raqdmOptions" = aqdmDefaults)
+  
+}
 
-  if(is.null(aqdmDefaults$user)) {
-    packageStartupMessage("\nU.S. EPA's Air Quaility Data Mart requires free registration. Please visit\n\n",
-                            "   http://www.epa.gov/airdata/tas_Data_Mart_Registration.html\n\n",
-                            "for information on registering. Once registered use\n\n",
-                            "   setAQDMUser()\n\n",
-                            "to set your username and password for use with raqdm.")
-  }
+.onAttach <- function(...) {
+
+  packageStartupMessage("\nU.S. EPA's Air Quaility Data Mart requires free registration. Please visit\n\n",
+                          "   http://www.epa.gov/airdata/tas_Data_Mart_Registration.html\n\n",
+                          "for information on registering. Once registered use\n\n",
+                          "   setAQDMUser()\n\n",
+                          "to set your username and password for use with raqdm.")
   
 }
 
